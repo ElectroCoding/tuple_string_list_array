89833506b3aab5b073fbd2ee45e7860cbadb9a56
ElectroCoding <electrocoding@gmail.com>
2023-11-25 08:09:12 -0800

    Create tuple_string_list_array.py
    This will print the length of:
    1) a tuple
    2) a string
    3) a list
    4) an array

---
+++ tuple_string_list_array.py
@@ -0,0 +1,25 @@
+# tuple_vs_string_vs_list_vs_array
+
+tuple_1 = (27, True, "car", "house", "bike")
+print(len(tuple_1))
+print()
+
+single_tuple = ("car", )
+print(len(single_tuple))
+print()
+
+string_1 = ("car")
+print(len(string_1))
+print()
+
+list_1 = [27, True, "car", "house", "bike"]
+print(len(list_1))
+print()
+
+list_1 = [27]
+print(len(list_1))
+print()
+
+import array as my_array_1
+array_1 = my_array_1.array('i', [1, 45, 63, 54])
+print(len(array_1))
