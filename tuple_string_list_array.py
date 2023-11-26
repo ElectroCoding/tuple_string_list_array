4ee536a0add04132c4dcc418b15dd80768a6d095
electrocoding <electrocoding@gmail.com>
2023-11-26 05:54:32 -0800

    Update tuple_string_list_array.py
    RL - added comment at line 2 to check the commit to github.

--- tuple_string_list_array.py
+++ tuple_string_list_array.py
@@ -1,25 +1,43 @@
 # tuple_vs_string_vs_list_vs_array
+# checking github for updates
 
+print()
+print("========================================")
 tuple_1 = (27, True, "car", "house", "bike")
 print(f"The length of the tuple is = {len(tuple_1)}")
+print(f"The type of the tuple is = {type(tuple_1)}")
+print("========================================")
 print()
 
 single_tuple = ("car", )
-print(len(single_tuple))
+print(f"The length of the single_tuple  is = {len(single_tuple )}")
+print(f"The type of the single_tuple  is = {type(single_tuple )}")
+print("========================================")
 print()
 
 string_1 = ("car")
-print(len(string_1))
+print(f"The length of string_1 is = {len(string_1)}")
+print(f"The type of string_1 is = {type(string_1)}")
+print("========================================")
 print()
 
 list_1 = [27, True, "car", "house", "bike"]
-print(len(list_1))
+print(f"The length of list_1 is = {len(list_1)}")
+print(f"The type of list_1 is = {type(list_1)}")
+print("========================================")
 print()
 
-list_1 = [27]
-print(len(list_1))
+list_2 = [27]
+print(f"The length of list_2 is = {len(list_2)}")
+print(f"The type of list_2 is = {type(list_2)}")
+print("========================================")
 print()
 
 import array as my_array_1
 array_1 = my_array_1.array('i', [1, 45, 63, 54])
-print(len(array_1))
+print(f"The length of array_1 is = {len(array_1)}")
+print(f"The type of array_1 is = {type(array_1)}")
+#  print(f"The Data_type of array_1 is = {array_1.dtype}")
+print()
+print("========================================")
+
